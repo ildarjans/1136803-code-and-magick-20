49357f18e0751cb149a07764dfa307e7f2d556bd
ildarjan <nobap84@rambler.ru>
2020-06-06 02:00:37 +0400

    own refactor

--- js/setup.js
+++ js/setup.js
@@ -4,23 +4,23 @@ var playerNames = ['Иван', 'Хуан Себастьян', 'Мария', 'К�
 var playerSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizardTemplate = document.querySelector('template#similar-wizard-template').content.querySelector('.setup-similar-item');
+var wizardInnerTemplateContent = document.querySelector('template#similar-wizard-template').content.querySelector('.setup-similar-item');
 var setup = document.querySelector('.setup');
 var setupSimilar = setup.querySelector('.setup-similar');
 var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
 
 setup.classList.remove('hidden');
-renderSetupWindow(wizardTemplate, playerNames, playerSurnames, wizardCoatColors, wizardEyesColors, 4);
+renderSetupWindow(wizardInnerTemplateContent, playerNames, playerSurnames, wizardCoatColors, wizardEyesColors, 4);
 setupSimilar.classList.remove('hidden');
 
-function renderSetupWindow(template, names, surnames, coatColors, eyesColors, quantity) {
+function renderSetupWindow(innerTemplateContent, names, surnames, coatColors, eyesColors, quantity) {
   var shortPlayerArray = getRandomArray(quantity, names);
   var shortCoatsArray = getRandomArray(quantity, coatColors);
   var shortEyesArray = getRandomArray(quantity, eyesColors);
   var playersFullNames = concatSurnamesWithExistingNames(names, shortPlayerArray, surnames);
 
   for (var i = 0; i < quantity; i++) {
-    var templateClone = template.cloneNode(true);
+    var templateClone = innerTemplateContent.cloneNode(true);
     renderWizardName(templateClone, playersFullNames[i], 'p.setup-similar-label');
     renderWizardColorDetail(templateClone, shortCoatsArray[i], '.wizard .wizard-coat');
     renderWizardColorDetail(templateClone, shortEyesArray[i], '.wizard .wizard-eyes');
@@ -30,28 +30,27 @@ function renderSetupWindow(template, names, surnames, coatColors, eyesColors, qu
 function getRandomArray(quantity, names) {
   var randomArray = [];
   while (randomArray.length < quantity) {
-    var guess = names[randomInt(names.length - 1)];
+    var guess = names[randIntUntil(names.length - 1)];
     if (randomArray.indexOf(guess) < 0) {
       randomArray.push(guess);
     }
   }
   return randomArray;
-
 }
-function concatSurnamesWithExistingNames(namesFullArray, namesShortArray, surnames) {
+function concatSurnamesWithExistingNames(namesFullArray, namesShortArray, surnamesFullArray) {
   return namesShortArray.map(function (name) {
-    return name + ' ' + surnames[namesFullArray.indexOf(name)];
+    return name + ' ' + surnamesFullArray[namesFullArray.indexOf(name)];
   });
 }
-function renderWizardName(template, name, selector) {
-  var element = template.querySelector(selector);
+function renderWizardName(innerTemplateContent, name, selector) {
+  var element = innerTemplateContent.querySelector(selector);
   element.textContent = name;
 }
-function renderWizardColorDetail(template, color, selector) {
-  var element = template.querySelector(selector);
+function renderWizardColorDetail(innerTemplateContent, color, selector) {
+  var element = innerTemplateContent.querySelector(selector);
   element.style.fill = color;
 }
-function randomInt(max) {
+function randIntUntil(max) {
   var ans = Math.floor(Math.random() * (max + 1));
   return ans;
 }
