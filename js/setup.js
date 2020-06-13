47bba0a4873a2079f967982288463e2e6732d72b
ildarjan <nobap84@rambler.ru>
2020-06-13 19:25:08 +0400

    1st review

--- js/setup.js
+++ js/setup.js
@@ -1,56 +1,89 @@
 'use strict';
 
-var playerNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var playerSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var playerNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var playerSurnames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizardInnerTemplateContent = document.querySelector('template#similar-wizard-template').content.querySelector('.setup-similar-item');
+var wizardInnerTemplateContent = document.querySelector('#similar-wizard-template')
+                                .content.querySelector('.setup-similar-item');
 var setup = document.querySelector('.setup');
 var setupSimilar = setup.querySelector('.setup-similar');
 var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
 
 setup.classList.remove('hidden');
-renderSetupWindow(wizardInnerTemplateContent, playerNames, playerSurnames, wizardCoatColors, wizardEyesColors, 4);
+renderSetupWindow(wizardInnerTemplateContent, 4);
 setupSimilar.classList.remove('hidden');
 
-function renderSetupWindow(innerTemplateContent, names, surnames, coatColors, eyesColors, quantity) {
-  var shortPlayerArray = getRandomArray(quantity, names);
-  var shortCoatsArray = getRandomArray(quantity, coatColors);
-  var shortEyesArray = getRandomArray(quantity, eyesColors);
-  var playersFullNames = concatSurnamesWithExistingNames(names, shortPlayerArray, surnames);
+function renderSetupWindow(wizardTemplate, qty) {
+  var players = getShortRandomArray(playerNames, qty);
+  var coats = getShortRandomArray(wizardCoatColors, qty);
+  var eyes = getShortRandomArray(wizardEyesColors, qty);
+  var fullNames = concatSurnamesWithExistingNames(playerNames, players, playerSurnames);
+  var documentFragment = document.createDocumentFragment();
 
-  for (var i = 0; i < quantity; i++) {
-    var templateClone = innerTemplateContent.cloneNode(true);
-    renderWizardName(templateClone, playersFullNames[i], 'p.setup-similar-label');
-    renderWizardColorDetail(templateClone, shortCoatsArray[i], '.wizard .wizard-coat');
-    renderWizardColorDetail(templateClone, shortEyesArray[i], '.wizard .wizard-eyes');
-    setupSimilarList.appendChild(templateClone);
+  for (var i = 0; i < qty; i++) {
+    var template = wizardTemplate.cloneNode(true);
+    renderWizardName(template, fullNames[i], 'p.setup-similar-label');
+    renderWizardColorDetail(template, coats[i], '.wizard .wizard-coat');
+    renderWizardColorDetail(template, eyes[i], '.wizard .wizard-eyes');
+    documentFragment.append(template);
   }
+  setupSimilarList.append(documentFragment);
 }
-function getRandomArray(quantity, names) {
-  var randomArray = [];
-  while (randomArray.length < quantity) {
-    var guess = names[randIntUntil(names.length - 1)];
-    if (randomArray.indexOf(guess) < 0) {
-      randomArray.push(guess);
+
+function getShortRandomArray(names, qty) {
+  if (names.length < qty) {
+    return undefined;
+  }
+  var randomNames = [];
+  while (randomNames.length < qty) {
+    var guess = names[getRandomInteger(names.length - 1)];
+    if (randomNames.indexOf(guess) < 0) {
+      randomNames.push(guess);
     }
   }
-  return randomArray;
+  return randomNames;
 }
-function concatSurnamesWithExistingNames(namesFullArray, namesShortArray, surnamesFullArray) {
-  return namesShortArray.map(function (name) {
-    return name + ' ' + surnamesFullArray[namesFullArray.indexOf(name)];
+
+function concatSurnamesWithExistingNames(names, shortNames, surnames) {
+  return shortNames.map(function (name) {
+    return name + ' ' + surnames[names.indexOf(name)];
   });
 }
-function renderWizardName(innerTemplateContent, name, selector) {
-  var element = innerTemplateContent.querySelector(selector);
-  element.textContent = name;
+
+function renderWizardName(template, name, selector) {
+  template.querySelector(selector).textContent = name;
 }
-function renderWizardColorDetail(innerTemplateContent, color, selector) {
-  var element = innerTemplateContent.querySelector(selector);
-  element.style.fill = color;
+
+function renderWizardColorDetail(template, color, selector) {
+  template.querySelector(selector).style.fill = color;
 }
-function randIntUntil(max) {
-  var ans = Math.floor(Math.random() * (max + 1));
-  return ans;
+
+function getRandomInteger(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
