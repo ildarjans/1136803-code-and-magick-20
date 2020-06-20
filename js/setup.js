f922ad710dcb35c7995f9f47dec521aeff17c9f1
ildarjan <nobap84@rambler.ru>
2020-06-20 19:19:55 +0400

    already done code have been wrapped in separate modules. Also in modal.js has module5-task1

--- js/setup.js
+++ js/setup.js
@@ -1,165 +1,72 @@
 'use strict';
 
-var playerNames = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
-var playerSurnames = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-                                .content.querySelector('.setup-similar-item');
-var setup = document.querySelector('.setup');
-var setupSimilar = setup.querySelector('.setup-similar');
-var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
+(function () {
+  var playerNames = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
+  var playerSurnames = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
+  var wizardCoatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+                                  .content.querySelector('.setup-similar-item');
+  var setup = document.querySelector('.setup');
+  var setupSimilar = setup.querySelector('.setup-similar');
+  var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
 
-renderSetupWindow(similarWizardTemplate, 4);
-// setup.classList.remove('hidden');
-setupSimilar.classList.remove('hidden');
+  var getShuffledArray = window.utilities.getShuffledArray;
 
-function renderSetupWindow(wizardTemplate, quantity) {
-  var players = getShuffledArray(playerNames, quantity);
-  var coats = getShuffledArray(wizardCoatColors, quantity);
-  var eyes = getShuffledArray(wizardEyesColors, quantity);
-  var fullNames = concatSurnamesWithExistingNames(playerNames, players, playerSurnames);
-  var documentFragment = document.createDocumentFragment();
+  renderSetupModal(similarWizardTemplate, 4);
+  // setup.classList.remove('hidden');
+  setupSimilar.classList.remove('hidden');
 
-  for (var i = 0; i < quantity; i++) {
-    var template = wizardTemplate.cloneNode(true);
-    template.querySelector('p.setup-similar-label').textContent = fullNames[i];
-    template.querySelector('.wizard .wizard-coat').style.fill = coats[i];
-    template.querySelector('.wizard .wizard-eyes').style.fill = eyes[i];
-    documentFragment.append(template);
-  }
-  setupSimilarList.append(documentFragment);
-}
+  function renderSetupModal(wizardTemplate, quantity) {
+    var players = getShuffledArray(playerNames, quantity);
+    var coats = getShuffledArray(wizardCoatColors, quantity);
+    var eyes = getShuffledArray(wizardEyesColors, quantity);
+    var fullNames = concatSurnamesWithExistingNames(playerNames, players, playerSurnames);
+    var documentFragment = document.createDocumentFragment();
 
-function getShuffledArray(array, quantity) {
-  if (array.length < quantity) {
-    return undefined;
-  }
-  quantity = !quantity ? array.length : quantity;
-  var randomNames = [];
-  while (randomNames.length < quantity) {
-    var guess = array[getRandomInteger(array.length - 1)];
-
-    if (randomNames.indexOf(guess) < 0) {
-      randomNames.push(guess);
+    for (var i = 0; i < quantity; i++) {
+      var template = wizardTemplate.cloneNode(true);
+      template.querySelector('p.setup-similar-label').textContent = fullNames[i];
+      template.querySelector('.wizard .wizard-coat').style.fill = coats[i];
+      template.querySelector('.wizard .wizard-eyes').style.fill = eyes[i];
+      documentFragment.append(template);
     }
+    setupSimilarList.append(documentFragment);
   }
-  return randomNames;
-}
 
-function concatSurnamesWithExistingNames(names, shortNames, surnames) {
-  return shortNames.map(function (name) {
-    return name + ' ' + surnames[names.indexOf(name)];
-  });
-}
-
-function getRandomInteger(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-// ######################################################
-// #####               MODULE4-TASK1                #####
-// ######################################################
-
-var fireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
-
-var setupOpenButton = document.querySelector('.setup-open');
-var setupCloseButton = setup.querySelector('.setup-close');
-var wizardCoat = setup.querySelector('.wizard .wizard-coat');
-var wizardEyes = setup.querySelector('.wizard .wizard-eyes');
-var fireballWrap = setup.querySelector('.setup-fireball-wrap');
-var fireballInputColor = fireballWrap.querySelector('input');
-var usernameInput = setup.querySelector('.setup-user-name');
-
-setupOpenButton.addEventListener('click', function () {
-  openSetupModal();
-});
-
-setupOpenButton.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-    openSetupModal();
+  function concatSurnamesWithExistingNames(names, shortNames, surnames) {
+    return shortNames.map(function (name) {
+      return name + ' ' + surnames[names.indexOf(name)];
+    });
   }
-});
+  window.setupModal = {
+    wizardCoatColors: wizardCoatColors,
+    wizardEyesColors: wizardEyesColors
+  };
 
-setupCloseButton.addEventListener('click', closeSetupModal);
-
-function openSetupModal() {
-  setup.classList.remove('hidden');
-  document.addEventListener('keydown', closeSetupEscapeHandler);
-  fireballWrap.addEventListener('click', setFireballColor);
-  setupCloseButton.addEventListener('keydown', closeSetupEnterHandler);
-  wizardCoat.addEventListener('click', setWizardCoatColor);
-  wizardEyes.addEventListener('click', setWizardEyesColor);
-}
-
-function closeSetupModal() {
-  setup.classList.add('hidden');
-  document.removeEventListener('keydown', closeSetupEscapeHandler);
-  fireballWrap.removeEventListener('click', setFireballColor);
-  setupCloseButton.removeEventListener('keydown', closeSetupEnterHandler);
-  wizardCoat.removeEventListener('click', setWizardCoatColor);
-  wizardEyes.removeEventListener('click', setWizardEyesColor);
-}
-
-function closeSetupEscapeHandler(event) {
-  if (event.key === 'Escape' && document.activeElement !== usernameInput) {
-    closeSetupModal();
-  }
-}
-
-function closeSetupEnterHandler(event) {
-  if (event.key === 'Enter') {
-    closeSetupModal();
-  }
-}
-
-function setWizardCoatColor() {
-  var currentFill = wizardCoat.style.fill;
-  wizardCoat.style.fill = getNextValue(wizardCoatColors, currentFill);
-}
-
-function setWizardEyesColor() {
-  var currentFill = wizardEyes.style.fill;
-  wizardEyes.style.fill = getNextValue(wizardEyesColors, currentFill);
-}
-
-function setFireballColor() {
-  var color = getNextValue(fireballColors, fireballInputColor.value);
-  fireballWrap.style.background = color;
-  fireballInputColor.value = color;
-}
-
-function getNextValue(arr, currentValue) {
-  return arr[(arr.indexOf(currentValue) + 1) % arr.length];
-}
+})();
--- js/stat.js
+++ js/stat.js
@@ -1,94 +1,94 @@
 'use strict';
 
-var BACKGROUND_SHADOW_OFFSET = 10;
-var barProperties = {
-  BOTTOM: 250,
-  GAP: 50,
-  HEIGHT: 150,
-  MIN_HEIGHT: 30,
-  WIDTH: 40
-};
-var backgroundProperties = {
-  HEIGHT: 270,
-  WIDTH: 420,
-  OFFSETX: 100,
-  OFFSETY: 20
-};
-var textProperties = {
-  GAP: 12,
-  FONT: '16px PT mono'
-};
-var ColorEnum = {
-  TEXT: '#303030',
-  RED: '#ff0000',
-  WHITE: 'rgb(255, 255, 255)',
-  SHADOW: 'rgba(0, 0, 0, 0.7)'
-};
+(function () {
+  var BACKGROUND_SHADOW_OFFSET = 10;
+  var barProperties = {
+    BOTTOM: 250,
+    GAP: 50,
+    HEIGHT: 150,
+    MIN_HEIGHT: 30,
+    WIDTH: 40
+  };
+  var backgroundProperties = {
+    HEIGHT: 270,
+    WIDTH: 420,
+    OFFSETX: 100,
+    OFFSETY: 20
+  };
+  var textProperties = {
+    GAP: 12,
+    FONT: '16px PT mono'
+  };
+  var ColorEnum = {
+    TEXT: '#303030',
+    RED: '#ff0000',
+    WHITE: 'rgb(255, 255, 255)',
+    SHADOW: 'rgba(0, 0, 0, 0.7)'
+  };
 
+  var getRandomInteger = window.utilities.getRandomInteger;
 
-function getRandomInteger(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function renderPlayerName(ctx, obj, color) {
-  ctx.textBaseline = 'middle';
-  ctx.fillStyle = color;
-  ctx.fillText(obj.name, backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM + textProperties.GAP);
-}
-
-function renderPlayerScore(ctx, obj, color) {
-  ctx.textBaseline = 'middle';
-  ctx.fillStyle = color;
-  ctx.fillText(obj.score, backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM - obj.columnHeight - textProperties.GAP);
-}
-
-function renderResultBar(ctx, obj) {
-  ctx.fillStyle = obj.renderColor;
-  ctx.fillRect(backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM, barProperties.WIDTH, -obj.columnHeight);
-}
-
-function renderStatisticBackground(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, backgroundProperties.WIDTH, backgroundProperties.HEIGHT);
-}
-
-function writeTitle(ctx, text, color, lineNumber) {
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = color;
-  ctx.fillText(text, backgroundProperties.OFFSETX + textProperties.GAP * 2, backgroundProperties.OFFSETY + textProperties.GAP * lineNumber);
-}
-
-function getRandomBlueHSL() {
-  return 'hsl(240, ' + getRandomInteger(100) + '%, 50%, 1)';
-}
-
-function calcBarHeight(score, minScore, maxScore, minHeight) {
-  return Math.round((barProperties.HEIGHT - minHeight) * ((score - minScore) / (maxScore - minScore))) + minHeight;
-}
-
-window.renderStatistics = function (ctx, players, scores) {
-  var maxScore = Math.round(Math.max.apply(null, scores));
-  var minScore = Math.round(Math.min.apply(null, scores));
-  ctx.font = textProperties.FONT;
-  renderStatisticBackground(ctx, backgroundProperties.OFFSETX + BACKGROUND_SHADOW_OFFSET, backgroundProperties.OFFSETY + BACKGROUND_SHADOW_OFFSET, ColorEnum.SHADOW);
-  renderStatisticBackground(ctx, backgroundProperties.OFFSETX, backgroundProperties.OFFSETY, ColorEnum.WHITE);
-
-  for (var i = 0; i < players.length; i++) {
-    var columnHeight = calcBarHeight(scores[i], minScore, maxScore, barProperties.MIN_HEIGHT);
-    var renderColor = players[i] === 'Вы' ? renderColor = ColorEnum.RED : getRandomBlueHSL();
-    var player = {
-      name: players[i],
-      score: Math.round(scores[i]),
-      columnHeight: columnHeight,
-      renderColor: renderColor,
-      order: i
-    };
-
-    renderResultBar(ctx, player);
-    renderPlayerName(ctx, player, ColorEnum.TEXT);
-    renderPlayerScore(ctx, player, ColorEnum.TEXT);
+  function renderPlayerName(ctx, obj, color) {
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = color;
+    ctx.fillText(obj.name, backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM + textProperties.GAP);
   }
 
-  writeTitle(ctx, 'Ура вы победили!', ColorEnum.TEXT, 1);
-  writeTitle(ctx, 'Список результатов:', ColorEnum.TEXT, 3);
-};
+  function renderPlayerScore(ctx, obj, color) {
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = color;
+    ctx.fillText(obj.score, backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM - obj.columnHeight - textProperties.GAP);
+  }
+
+  function renderResultBar(ctx, obj) {
+    ctx.fillStyle = obj.renderColor;
+    ctx.fillRect(backgroundProperties.OFFSETX + barProperties.GAP * (obj.order + 1) + barProperties.WIDTH * obj.order, barProperties.BOTTOM, barProperties.WIDTH, -obj.columnHeight);
+  }
+
+  function renderStatisticBackground(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, backgroundProperties.WIDTH, backgroundProperties.HEIGHT);
+  }
+
+  function writeTitle(ctx, text, color, lineNumber) {
+    ctx.textBaseline = 'hanging';
+    ctx.fillStyle = color;
+    ctx.fillText(text, backgroundProperties.OFFSETX + textProperties.GAP * 2, backgroundProperties.OFFSETY + textProperties.GAP * lineNumber);
+  }
+
+  function getRandomBlueHSL() {
+    return 'hsl(240, ' + getRandomInteger(100) + '%, 50%, 1)';
+  }
+
+  function calcBarHeight(score, minScore, maxScore, minHeight) {
+    return Math.round((barProperties.HEIGHT - minHeight) * ((score - minScore) / (maxScore - minScore))) + minHeight;
+  }
+
+  window.renderStatistics = function (ctx, players, scores) {
+    var maxScore = Math.round(Math.max.apply(null, scores));
+    var minScore = Math.round(Math.min.apply(null, scores));
+    ctx.font = textProperties.FONT;
+    renderStatisticBackground(ctx, backgroundProperties.OFFSETX + BACKGROUND_SHADOW_OFFSET, backgroundProperties.OFFSETY + BACKGROUND_SHADOW_OFFSET, ColorEnum.SHADOW);
+    renderStatisticBackground(ctx, backgroundProperties.OFFSETX, backgroundProperties.OFFSETY, ColorEnum.WHITE);
+
+    for (var i = 0; i < players.length; i++) {
+      var columnHeight = calcBarHeight(scores[i], minScore, maxScore, barProperties.MIN_HEIGHT);
+      var renderColor = players[i] === 'Вы' ? renderColor = ColorEnum.RED : getRandomBlueHSL();
+      var player = {
+        name: players[i],
+        score: Math.round(scores[i]),
+        columnHeight: columnHeight,
+        renderColor: renderColor,
+        order: i
+      };
+
+      renderResultBar(ctx, player);
+      renderPlayerName(ctx, player, ColorEnum.TEXT);
+      renderPlayerScore(ctx, player, ColorEnum.TEXT);
+    }
+
+    writeTitle(ctx, 'Ура вы победили!', ColorEnum.TEXT, 1);
+    writeTitle(ctx, 'Список результатов:', ColorEnum.TEXT, 3);
+  };
+})();
+
